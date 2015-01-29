66c57144192cd5edf777872bccc705481f83d7a4
admin <chaluemwut@hotmail.com>
2015-01-29 23:03:31 +0700

    user pass word page

--- app/src/main/java/rmuti/myapplication/MainActivity.java
+++ app/src/main/java/rmuti/myapplication/MainActivity.java
@@ -2,8 +2,12 @@ package rmuti.myapplication;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +16,14 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button but = (Button)findViewById(R.id.button);
+        but.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Log.v("test","click me...");
+            }
+        });
     }
 
 
