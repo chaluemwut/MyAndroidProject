0d323500171e07bd5e128e73c28a39e4fccf68ff
chaluemwut <chaluemwut@hotmail.com>
2015-02-05 12:30:30 +0700

    lab 2

--- app/src/main/java/rmuti/myapplication/WeatherActivity.java
+++ app/src/main/java/rmuti/myapplication/WeatherActivity.java
@@ -1,17 +1,67 @@
 package rmuti.myapplication;
 
+import android.os.StrictMode;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
 
 
 public class WeatherActivity extends ActionBarActivity {
+
+    private final String api_call = "http://api.openweathermap.org/data/2.5/weather?q=";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Button but = (Button)findViewById(R.id.query);
+        but.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Log.d("test","Click....");
+                EditText qText = (EditText)findViewById(R.id.q_text);
+                String qMessage = qText.getText().toString();
+                try {
+                    URL url = new URL(api_call+qMessage);
+                    Scanner sc = new Scanner(url.openStream());
+                    StringBuffer buf = new StringBuffer();
+                    while(sc.hasNext()){
+                        buf.append(sc.next());
+                    }
+
+
+                    JSONObject jsonObject = new JSONObject(buf.toString());
+                    JSONObject sysObj = jsonObject.getJSONObject("sys");
+                    String country = sysObj.getString("country");
+
+                    TextView countryText = (TextView)findViewById(R.id.country_text);
+                    countryText.setText(country);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
     }
 
